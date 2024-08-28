cad24efae3676f16218e071b345067bd20c9c350
Rajesh Gupta <rajeshg8726@gmail.com>
2024-08-28 09:23:35 +0530

    cors updated for frontend

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 
 
-
 // Create the Express app
 const app = express();
 
@@ -17,46 +16,38 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Middleware
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
- 
+
 app.use(cors());
+app.use(cors({
+  origin: 'https://rgjobs.in/',
+}));
 // Serve static files from the 'public' directory
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
 // Connect to MongoDB
-mongoose.connect(MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => {
-    console.error('MongoDB connection error:', err);
-    process.exit(1);
+mongoose.connect(MONGO_URI).then(() => {
+  console.log('Successfully connected to MongoDB');
+}).catch((error) => {
+  console.error('MongoDB connection error:', error);
 });
-
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 
 
 // Creating the session
 app.use(cookieParser());
- 
 
-app.use(session({
-  secret: "rajeshg@8726",
-  saveUninitialized: true,
-  resave: true
-
-}));
+// use that route 
+app.use('/', adminRouter);
 
 
-  // use that route 
-  app.use('/' , adminRouter);
-  
- 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
